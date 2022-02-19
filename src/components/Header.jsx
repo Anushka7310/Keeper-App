fd4e6b4aaaa084a3f9e68035ac6c054d220a1be9
Anushka7310 <61081130+Anushka7310@users.noreply.github.com>
2022-02-19 14:50:04 +0530

    final touches

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
 
 function Header() {
-    return <header>
+    return (
+    <header>
 
-        <h1>Keeper</h1>
+        <h1>
+        <HighlightIcon />
+        Keeper</h1>
     </header>
+    );
 }
 
 
